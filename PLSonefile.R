2bcca6de5cd9e37ef740caa09741140123ecdb3a
J Bi <jbi@Js-MacBook-Pro.local>
2018-12-01 16:45:32 +0100

    init_luis
    adding predict.R , PLSonefile.R

---
+++ PLSonefile.R
@@ -0,0 +1,98 @@
+X_wine<-matrix(c(7, 7, 13, 7, 4, 3, 14, 7, 10, 5, 12, 5, 16, 7, 11, 3, 13, 3, 10, 3 ),5,4, byrow=TRUE)
+Y_wine<-matrix(c(14, 7, 8, 10, 7, 6, 8, 5, 5, 2, 4, 7, 6, 2, 4 ),5,3, byrow=TRUE)
+
+mX<-as.matrix(X_wine)
+mY<-as.matrix(Y_wine)
+
+mX<-scale(mX, center=TRUE, scale=TRUE)
+mY<-scale(mY, center=TRUE, scale=TRUE)
+
+
+complatentscores <- function(X,Y){
+  
+  u <- rnorm(length(mY[,1]))
+  
+  t <- NA
+  told <- NA
+  compteur <- 1
+  while(is.na(t) || is.na(told) || sum((t-told)^2)>(10^(-14))){
+    
+    told <- t
+    
+    x_weights <- t(X)%*%u
+    x_weights <- x_weights/sqrt(sum(x_weights^2))
+    
+    t <- X%*%x_weights
+    t <- t/sqrt(sum(t^2))
+
+    y_weights <- t(Y)%*%t
+    y_weights <- y_weights/sqrt(sum(y_weights^2))
+    
+    u <- Y%*%y_weights
+    
+   
+    
+    compteur <- compteur +1
+  }
+  result <- list(t_scores=t,u_scores=u,x_weights=x_weights,y_weights=y_weights)
+  return(result)
+  
+}
+
+#Number of comp to compute:
+comp.max=3
+#Matrix intilisation
+T.scores = matrix(ncol = comp.max,nrow = nrow(mX))
+U.scores = matrix(ncol = comp.max,nrow = nrow(mX))
+P.loadings = matrix(ncol=comp.max,nrow = ncol(mX))
+X.weights = matrix(ncol=comp.max,nrow=ncol(mX))
+Y.weights = matrix(ncol=comp.max,nrow=ncol(mY))
+B = matrix(0,ncol=comp.max,nrow=comp.max)
+
+E0=mX
+F0=mY
+for (i in 1:comp.max) {
+  latentlist <- complatentscores(E0,F0)
+  
+  T.scores[,i] <- latentlist$t_scores
+  U.scores[,i] <- latentlist$u_scores
+  X.weights[,i] <-latentlist$x_weights
+  Y.weights[,i] <- latentlist$y_weights
+  
+  b <- as.numeric(t(latentlist$t_scores)%*%latentlist$u_scores)
+  B[i,i] <- b
+  
+  p <- t(E0)%*%latentlist$t_scores
+  P.loadings[,i] <-p
+  
+  E0 <- E0-latentlist$t_scores%*%t(p)
+  F0 <- F0-b*(latentlist$t_scores%*%t(latentlist$y_weights))
+  
+  
+  
+}
+
+print("T matrix :")
+print(T.scores)
+print("U matrix :")
+print(U.scores)
+print("P loadings matrix :")
+print(P.loadings)
+print("X weights W matrix :")
+print(X.weights)
+print("Y weights C matrix :")
+print(Y.weights)
+print(" B vector")
+print(diag(B))
+
+#Need library MASS for pseudo inverse
+# predict1 <- function(Xnew,P.loadings,B,Y.weights){
+#   Bpls <- ginv(t(P.loadings))%*%B%*%t(Y.weights)
+#   print(Bpls)
+#   return(Xnew%*%Bpls)
+# }
+
+predict2 <- function(Xnew,P.loadings,X.weights,Y.weights){
+  Bpls <- X.weights%*%solve(t(P.loadings)%*%X.weights)%*%t(Y.weights)
+  return(Xnew%*%Bpls)
+}
---
+++ predict.R
@@ -0,0 +1,15 @@
+# function that predict the Y with new X observations
+
+#corpcor package is used to compute pseudoinverse matrix
+library(corpcor)
+
+predict <- function(Xnew, y_weights,p_loads,B){
+  p<-p_loads[-1]
+  print(p)
+  B<-B[2:nrow(B),2:ncol(B)]
+  Bpls <- pseudoinverse(t(p))%*%B%*%t(y_weights)
+  Ypred <- as.matrix(Xnew)%*%Bpls
+  return(Ypred)
+  
+}
+
