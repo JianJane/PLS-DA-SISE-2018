c98ba6098ca86acc1392f65320f16ead9b22a812
Luis <hello@luisventura.me>
2018-12-10 18:54:49 +0100

    added autoselection of variables

--- easyPLSDA/R/easyPLSDA.R
+++ easyPLSDA/R/easyPLSDA.R
@@ -1,8 +1,9 @@
-plsDA <- function(formula,data=NULL,ncomp=2,method="classic",tol=10^-9,scale=TRUE){
+plsDA <- function(formula,data=NULL,ncomp=2,method="classic",auto.select.var=TRUE,threshold=0.8,tol.conv=10^-9,scale=TRUE){
 
   instance <- list()
   extractedDF <- model.frame(formula,data)
   X <- extractedDF[,-1]
+  var.names.X <- colnames(X)
   Y <- extractedDF[,1]
 
   #Test on rank of matrix X
@@ -20,21 +21,27 @@ plsDA <- function(formula,data=NULL,ncomp=2,method="classic",tol=10^-9,scale=TRU
 
 
 
-  #
+  #Scale X if needed
   if(scale==TRUE){
-    Xscaled <- scale(X)
+    Xp <- scale(X)
+    instance$scaled <- TRUE
+
+  }else{
+    Xp <- X
+    instance$scaled <- FALSE
 
   }
 
-  computedPls <- pls2(Xscaled,Y,ncomp=ncomp,method=method,tol=tol)
+  computedPls <- pls2(Xp,Y,ncomp=ncomp,method=method,tol=tol.conv)
 
   #Computation of explained variance (taken from https://github.com/gastonstat/):
   R2.X <- colMeans(cor(X,computedPls$scores$X)^2)
   R2.Y <- colMeans(cor(Y,computedPls$scores$X)^2)
   Rd.mat = matrix(0,ncomp, ncomp)
-  for (j in 1:ncomp)
+  for (j in 1:ncomp){
     Rd.mat[1:j,j] = R2.Y[1:j]
-  # variable importance
+  }
+  # variable importance computation
   VIP = sqrt((computedPls$weights$X^2) %*% Rd.mat %*% diag(ncol(X)/cumsum(R2.Y), ncomp, ncomp))
 
   ### Weighted VIP for entire fitted model
@@ -42,10 +49,54 @@ plsDA <- function(formula,data=NULL,ncomp=2,method="classic",tol=10^-9,scale=TRU
   for (i in 1:length(R2.Y)){
     weighted.vip[,i] <- R2.Y[i] * (computedPls$weights$X[,i]^2)
   }
-  VIP.weighted <- sqrt(rowSums(weighted.vip) * (ncol(X)/sum(R2.Y) ))
-  # combine with individual component VIPs
+  VIP.weighted <- sqrt(rowSums(weighted.vip) * (ncol(X)/sum(R2.Y)))
+
+  #Autoslection of variables if needed
+  if(auto.select.var==T){
+    var.to.exclude <- VIP.weighted<threshold
+    instance$selected.var <- var.names.X[VIP.weighted>=threshold]
+    X <- X[,-var.to.exclude]
+    var.names.X <- colnames(X)
+    #Scale X if needed
+    if(scale==TRUE){
+      Xp <- scale(X)
+      instance$scaled <- TRUE
+
+    }else{
+      Xp <- X
+      instance$scaled <- FALSE
+
+    }
+
+
+    computedPls <- pls2(Xp,Y,ncomp=ncomp,method=method,tol=tol.conv)
+
+    #Computation of explained variance (taken from https://github.com/gastonstat/):
+    R2.X <- colMeans(cor(X,computedPls$scores$X)^2)
+    R2.Y <- colMeans(cor(Y,computedPls$scores$X)^2)
+    Rd.mat = matrix(0,ncomp, ncomp)
+    for (j in 1:ncomp){
+      Rd.mat[1:j,j] = R2.Y[1:j]
+    }
+    # variable importance computation
+    VIP  <- sqrt((computedPls$weights$X^2) %*% Rd.mat %*% diag(ncol(X)/cumsum(R2.Y), ncomp, ncomp))
+
+    ### Weighted VIP for entire fitted model
+    weighted.vip <- matrix(0, nrow = ncol(X), ncol=ncomp)
+
+
+  }
+
+  # combine weighted VIPs with individual component VIPs
+  for (i in 1:length(R2.Y)){
+    weighted.vip[,i] <- R2.Y[i] * (computedPls$weights$X[,i]^2)
+  }
+  VIP.weighted <- sqrt(rowSums(weighted.vip) * (ncol(X)/sum(R2.Y)))
   VIP <- cbind(VIP, VIP.weighted)
-  dimnames(VIP) = list(colnames(X), c(paste(rep("Component ",ncomp),1:ncomp,sep=""),"Model VIP"))
+  dimnames(VIP) = list(var.names.X, c(paste(rep("Component ",ncomp),1:ncomp,sep=""),"Model VIP"))
+
+
+
 
   #Creation of output
   instance$X <- X
